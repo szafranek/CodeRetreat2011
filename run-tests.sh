a0c2509d41be52e3da0f96c36d48e38f0c1b4e5f
Krzysztof Szafranek <ebraun@o2.pl>
2011-07-08 18:09:08 +0200

    Basic setup.

---
+++ run-tests.sh
@@ -0,0 +1,1 @@
+java -jar ~/work/tools/js-test-driver/JsTestDriver-1.3.2.jar --tests all --captureConsole --config jstd-config.cfg --reset
---
+++ start-server.sh
@@ -0,0 +1,4 @@
+echo "Starting server... Connect your browsers to http://localhost:9876"
+java -jar ~/work/tools/js-test-driver/JsTestDriver-1.3.2.jar --port 9876
+
+
