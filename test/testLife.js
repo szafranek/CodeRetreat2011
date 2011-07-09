a9b12a9d608b075fa547b55a35a92e3653c35f8f
Krzysztof Szafranek <ebraun@o2.pl>
2011-07-09 09:07:12 +0200

    Default, running settings for JST

---
+++ test/testLife.js
@@ -0,0 +1,17 @@
+/**
+ * TestCase for testing of Life
+ **/
+var LifeTest = TestCase("LifeTest", {
+
+    setUp: function() {
+        
+    },
+  
+    tearDown: function() {
+    
+    },
+
+    testTruth: function() {
+        assertTrue(true);
+    }
+});
